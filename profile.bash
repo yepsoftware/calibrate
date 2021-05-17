9c5952daea9f83c27535fe9ce13dbb9f8992cced
Paul BUELENS <buelens.paul@gmail.com>
2021-05-17 09:56:13 +0200

    Added logfile

--- profile.bash
+++ profile.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 
 #############################################################################
@@ -12,13 +12,19 @@ function pressEnter {
 #############################################################################
 function log {
 #############################################################################
-   echo "$(date +'%Y%m%d %H:%M:%S,%N') $*"
+   if [[ "${logfn}" != "" ]] && [[ -f "${logfn}" ]];then
+      # output to logfile + stdout
+      echo "$(date +'%Y%m%d %H:%M:%S,%N') $*" | tee -a ${logfn}
+   else
+      # logfile is not yet there, only output to stdout
+      echo "$(date +'%Y%m%d %H:%M:%S,%N') $*"
+   fi
 }
 
 #############################################################################
 function edit_params {
 #############################################################################
-# $1: Tekst
+# $1: Text
 # $2: Default parameters
    echo ""
    echo "$1"
@@ -41,7 +47,7 @@ function do_init {
    while [[ "${dir}" = "" ]];
    do
       echo ""
-      echo -n "Enter directory where to save profile (default= ~/Documents): "
+      echo -n "Enter directory where to save profile (default=${HOME}/Documents): "
       read dir
       if [[ "${dir}" = "" ]];then
          dir="${HOME}/Documents"
@@ -58,6 +64,9 @@ function do_init {
 
    targetdir=${dir}/${nm}_${dt}
    mkdir -p ${targetdir}    # store all files for this run in this directory
+
+   logfn=${targetdir}/profile.log
+   touch ${logfn}
 
    # set default params
 
@@ -92,19 +101,16 @@ function do_init {
    ccmx_default="${base}/color_correction_matrix/ColorMunki_Display_Eizo_CS270.ccmx"
 }
 
-
 #############################################################################
 function do_calibrate {
 #############################################################################
    # Calibrate
    #----------
-   
-   #ccmx="${base}/color_correction_matrix/i1 DisplayPro, ColorMunki Display & Eizo CS270 (i1 Pro).ccmx"
-   #dispcal -v -d1 -qh -t6500 -b100 -g2.2 -X$ccmx ${nm}_${dt}
 
    #dispcal_params_default="-v -d1 -qh -t6500 -b100 -g2.2 ${nm}_${dt}"
    echo "RUNNING: dispcal ${dispcal_params}"
    dispcal ${dispcal_params}
+   last_argyll_rc=$?
 }   
    
 #############################################################################
@@ -112,10 +118,11 @@ function do_genTargets {
 #############################################################################
    # Generate profiling test targets
    #--------------------------------
-   
+
    #targen_params_default="-v -g16 -d3 ${targetdir}/${nm}_${dt}"
    echo "RUNNING: targen ${targen_params}"
    targen ${targen_params}
+   last_argyll_rc=$?
 }
 
 #############################################################################
@@ -123,10 +130,11 @@ function do_profile {
 #############################################################################
    # Profile
    #--------
-   
+
    #dispread_params_default="-v -d1 -k ${targetdir}/${nm}_${dt}.cal ${targetdir}/${nm}_${dt}"
    echo "RUNNING: dispread ${dispread_params}"
    dispread ${dispread_params}
+   last_argyll_rc=$?
 }
 
 #############################################################################
@@ -134,10 +142,11 @@ function do_icc {
 #############################################################################
    # Generate ICC profile
    #---------------------
-   
+
    #colprof_params_default="-v -qh -as -nc ${targetdir}/${nm}_${dt}"
    echo "RUNNING: colprof ${colprof_params}"
    colprof ${colprof_params}
+   last_argyll_rc=$?
 }   
 
 #############################################################################
@@ -164,17 +173,19 @@ function do_menu {
       echo ""
       echo -n "Your choice: "
       read choice
+      echo ""
       case $choice in
       0)
          stop="true"
          ;;
       1)
-         log "----- Initializing ..."
+         log "----- Start initializing ..." 
          do_init
          log "Initialization finished"
+         log "Continue with step 2 after checking for warnings/errors"
          ;;
       2)
-         log "----- Starting calibration"
+         log "----- Start calibration" 
   
          echo "***WARNING*** add parameter -y1 ( y 'one' ) for Lenovo legion laptop !"
          edit_params "Provide parameters for dispcal" "${dispcal_params_default}"
@@ -183,8 +194,11 @@ function do_menu {
          echo ""
          echo "You can check the below URL for a color correction matrix for your 'screen/measuring device' combination:"
          echo "    https://colorimetercorrections.displaycal.net/"
-         echo "If you do not find one, reply 'none' (without the quotes) to the question below."
-         echo "Note: ccmx file MUST NOT HAVE spaces/commas/brackets/ampersands in the name !!!"
+         echo ""
+         echo "If you do not find one or do not want to use one, "
+         echo "   reply 'none' (without the quotes) to the question below."
+         echo ""
+         echo "Note: the ccmx file MUST NOT HAVE spaces/commas/brackets/ampersands in the name !!!"
          echo "      rename the file first or create a link if needed !!!"
          
          edit_params "Provide ccmx file if you have one, enter 'none' otherwise" "$ccmx_default"
@@ -197,36 +211,47 @@ function do_menu {
                log "***WARNING*** Color correction matrix file [${params}] does not exist, ignoring."
             fi
          fi
-         do_calibrate
+         do_calibrate > >(tee -a ${logfn}) 2>&1     # process substitution
          log "output: ${targetdir}/${nm}_${dt}.cal"
-         log "Calibration finished, .cal created"
+         if [[ ${last_argyll_rc} -ne 0 ]];then
+            log "***WARNING*** Argyll rc=${last_argyll_rc}, check for errors ! See also ${logfn}"
+         fi
+         log "Continue with step 3 after checking for warnings/errors"
          ;;
       3)
-         log "----- Starting to generate target"
+         log "----- Start to generate target" 
          edit_params "Provide parameters for targen" "${targen_params_default}"
          targen_params=${params}
-         do_genTargets
-         log "input : ${targetdir}/${nm}_${dt}.cal"
+         do_genTargets > >(tee -a ${logfn}) 2>&1     # process substitution
+         log "input : ${targetdir}/${nm}_${dt}.cal" 
          log "output: ${targetdir}/${nm}_${dt}.ti1"
-         log "Target generated, .ti1 created"
+         if [[ ${last_argyll_rc} -ne 0 ]];then
+            log "***WARNING*** Argyll rc=${last_argyll_rc}, check for errors ! See also ${logfn}"
+         fi
+         log "Continue with step 4 after checking for warnings/errors"
          ;;
       4)
-         log "----- Starting profiling"
+         log "----- Start profiling"
          edit_params "Provide parameters for dispread" "${dispread_params_default}"
          dispread_params=${params}
-         do_profile
+         do_profile > >(tee -a ${logfn}) 2>&1     # process substitution
          log "input : ${targetdir}/${nm}_${dt}.ti1"
          log "output: ${targetdir}/${nm}_${dt}.ti3"
-         log "Profiling finished, .ti3 created"
+         if [[ ${last_argyll_rc} -ne 0 ]];then
+            log "***WARNING*** Argyll rc=${last_argyll_rc}, check for errors ! See also ${logfn}"
+         fi
+         log "Continue with step 5 after checking for warnings/errors"
          ;;
       5)
-         log "----- Starting icc generation"
+         log "----- Start icc generation"
          edit_params "Provide parameters for colprof" "${colprof_params_default}"
          colprof_params=${params}
-         do_icc
+         do_icc > >(tee -a ${logfn}) 2>&1     # process substitution
          log "input : ${targetdir}/${nm}_${dt}.ti3"
          log "output: ${targetdir}/${nm}_${dt}.icc"
-         log "icc profile created, .icc created"
+         if [[ ${last_argyll_rc} -ne 0 ]];then
+            log "***WARNING*** Argyll rc=${last_argyll_rc}, check for errors ! See also ${logfn}"
+         fi
          ;;
       *)
          echo "***ERROR*** Invalid choice"
@@ -247,22 +272,26 @@ nm="" # name of profile
 dir="" # directory where profile will be saved
 params=""
 
-# default parameters
+# default parameters for argyll commands
 dispcal_params_default=""
 targen_params_default=""
 dispread_params_default=""
 colprof_params_default=""
 ccmx_default=""
 
-# actual parameters
+# actual parameters for argyll commands
 dispcal_params=""
 targen_params=""
 dispread_params=""
-colprof_params=A
+colprof_params=""
 ccmx=""
 
+targetdir="" # where all output goes, incl .icc profile
+logfn="" # full path of the log file
 
-stop="false"
+last_argyll_rc=0  # rc from argyll commands
+
+stop="false"   # when set to 'true', then exit menu
 base=$(dirname $0)
 
 do_menu
